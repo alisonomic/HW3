8ff0f153f20928a631983ecfd974dd07237caa95
Alibaba <alisona83@gmail.com>
2020-04-21 00:02:34 -0400

    complete

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ function writePassword() {
     passwordText.value = password;
 
 }
-//Array options
-var letterCap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var letterLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "~", "?"];
 
 //Variables
 var confirmLength = "";
@@ -21,6 +16,12 @@ var confirmUpper;
 var confirmLower;
 var confirmNumbers;
 var confirmSpecialChar;
+
+//Array options
+var letterCap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var letterLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "~", "?"];
 
 //Initial prompt
 function generatePassword() {
